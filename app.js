edffb8e165f6bb4be0d01f86a32f9e8346d79df1
Akkowicz <uamfhq@gmail.com>
2018-07-14 16:57:47 +0200

    Change URL parser

--- app.js
+++ app.js
@@ -5,18 +5,19 @@ const movies = require('./routes/movies');
 const comments = require('./routes/comments');
 
 const PORT = process.env.PORT || 5000;
-const DB = process.env.DBHOST || 'mongodb://localhost/test';
+const DB = 'mongodb://localhost:27017/test';
 const app = express();
 
-mongoose.connect(DB);
+mongoose.connect(DB, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error:'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json' }));
 
 app.get('/', (req, res) => res.json({ message: 'Welcome to our Movie API! Available routes: GET /movies, GET /comments, POST /movies [Movie Title], POST /comments [Movie ID]' }));
-
 app.route('/movies')
   .get(movies.getMovies)
   .post(movies.addMovie);
